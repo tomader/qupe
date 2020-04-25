4af9a4901d7e14adfec66da3e647ffdfa07f6e40
tomader <45298082+tomader@users.noreply.github.com>
2020-04-25 19:57:10 +0300

    refactoring

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php
-phpinfo();
+session_start();
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+	$uri = 'https://';
+} else {
+	$uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+include 'backend/common.php';
+if (isset($_SESSION["login"])) {
+	$role = (int) $_SESSION["role"];
+	$page = '';
+	switch ($role) {
+		case Roles::Admin:
+			$page = "adminIndex.html";
+			break;
+		case Roles::Head:
+			$page = "";
+			break;
+		case Roles::Quality:
+			$page = "";
+			break;
+		case Roles::Faculty:
+			$page = "index.html";
+			break;
+	}
+	header('Location: ' . $uri . '/gp2/' . $page . '');
+	exit;
+} else {
+	header('Location: ' . $uri . '/gp2/login.html');
+	exit;
+}
+
 ?>
+Something is wrong with the XAMPP installation :-(
